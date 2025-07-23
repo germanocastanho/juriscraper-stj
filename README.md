1bc3a7600e99d21d4881d347a581b3d22d448e88
Germano Castanho <germanocastanho@proton.me>
2025-07-23 00:28:37 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# 🎯 JuriScraper STJ
+
+**Scraper** focused on extracting case summaries from the **Superior Tribunal de Justiça (STJ)**. A simple yet powerful script designed to assist legal professionals in jurisprudence research, saving extracted summaries in Markdown format to facilitate integration with Artificial Intelligence (AI) models. Useful for those who want to analyze legal data or build applications that require access to STJ case summaries. ⚖️
+
+<div align="center">
+  <img style="max-width: 100%; height: auto;" src="assets/court.jpg" alt="Brasília/DF, 09/11/2024 - Plenary Session, by Emerson Leal" />
+  <p><i>Also known as the "Citizenship Court," in reference to the Citizen Constitution (CF/88), the STJ is the guardian of brasilian <b>federal legislation</b>. It is responsible for the standardization and interpretation of this legislation, as well as acting as an ordinary and extraordinary instance in cases of various natures (art. 105, CF/88).</i></p>
+</div>
+
+# 🚀 Main Features
+
+- **Case Summary Extraction:** 🕸️ Automated collection of STJ case summaries, facilitating analysis.
+- **Markdown Format:** 📄 Generation of Markdown files, enabling seamless integration with AI models.
+- **Minimalist Interface (CLI):** 🎨 Command-line interaction, providing practicality for searches.
+
+# ✅ Prerequisites
+
+- **Python 3.12+**, available through the [**official website**](https://www.python.org/downloads/).
+
+# 🛠️ Local Installation
+
+```bash
+# Clone the repository
+git clone https://github.com/germanocastanho/juriscraper-stj.git
+
+# Navigate to the directory
+cd juriscraper-stj
+
+# Set up a virtual environment
+python3 -m venv .venv
+source .venv/bin/activate
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Run the "main.py" script
+python3 main.py
+```
+
+# 📜 Free Software
+
+Distributed under the [**GNU GPL v3**](LICENSE), ensuring freedom - as in "free speech" - to use, modify, and redistribute the software, as long as these freedoms are preserved in any derivative versions. By using or contributing, you support the **free software** philosophy and help build a libertarian technological environment! ✊
---
+++ main.py
@@ -0,0 +1,114 @@
+# Copyleft 🄯 2025, Germano Castanho
+# Free software under the GNU GPL v3
+
+
+import time
+import uuid
+from pathlib import Path
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+HEADNOTES = Path(__file__).parent / "data"
+HEADNOTES.mkdir(exist_ok=True)
+
+
+def get_user_keywords():
+    print("Welcome to JuriScraper STJ! 🎯")
+    time.sleep(1)
+
+    keywords = input("What shall we research? 🔍\n")
+    return keywords
+
+
+def access_stj_webpage():
+    print("Accessing the Court website... 💻")
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--disable-notifications")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--user-agent=Chrome/134.0")
+    options.add_argument("--log-level=3")
+
+    driver = webdriver.Chrome(options=options)
+    driver.get("https://scon.stj.jus.br/SCON/")
+    time.sleep(3)
+    return driver
+
+
+def search_user_keywords(driver, keywords):
+    print("Searching for relevant headnotes... 🔎")
+    text_input = driver.find_element(By.ID, "pesquisaLivre")
+    text_input.send_keys(keywords)
+    text_input.send_keys(Keys.RETURN)
+    time.sleep(3)
+    return None
+
+
+def get_max_headnotes_per_page(driver):
+    print("Configuring headnotes selection... ⚙️")
+    select_box = driver.find_element(By.ID, "qtdDocsPagina")
+    options = select_box.find_elements(By.TAG_NAME, "option")
+    max_value = max(
+        options,
+        key=lambda x: int(x.get_attribute("value")),
+    )
+    max_value.click()
+    time.sleep(3)
+    return None
+
+
+def extract_stj_headnotes(driver):
+    print("Extracting Court headnotes... 📜")
+    page_source = driver.page_source
+    soup = BeautifulSoup(page_source, "html.parser")
+    titles = soup.find_all("div", class_="docTitulo")
+    texts = soup.find_all("div", class_="docTexto")
+
+    notes_list = []
+    for title, text in zip(titles, texts):
+        key, value = title.text, text.text
+        notes_dict = {key: value}
+        if "Ementa" in notes_dict:
+            notes_list.append(notes_dict["Ementa"])
+
+    headnotes = "".join(notes_list)
+    time.sleep(3)
+    return headnotes
+
+
+def save_stj_headnotes(headnotes):
+    print('Saving headnotes to "data"... 📥')
+    notes_file = HEADNOTES / f"{uuid.uuid4()}.md"
+    with open(notes_file, "w", encoding="utf-8") as f:
+        f.write(headnotes)
+
+    time.sleep(3)
+    return None
+
+
+def exit_web_browser(driver):
+    print("Closing JuriScraper... See you later! 👋")
+    driver.quit()
+    time.sleep(1)
+    return None
+
+
+def main():
+    keywords = get_user_keywords()
+    driver = access_stj_webpage()
+    search_user_keywords(driver, keywords)
+    get_max_headnotes_per_page(driver)
+    headnotes = extract_stj_headnotes(driver)
+    save_stj_headnotes(headnotes)
+    exit_web_browser(driver)
+    return None
+
+
+if __name__ == "__main__":
+    main()
